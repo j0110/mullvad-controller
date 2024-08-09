ba68f41a85b4349278ed0d8c2b73e21e701bca30
j0110 <jacquesferrand03@gmail.com>
2024-08-09 16:39:41 +0200

    mistake

--- main.py
+++ main.py
@@ -218,7 +218,7 @@ def main():
         if answer == "3":
             update()
         if answer == "4":
-            intall_shortcut()
+            install_shortcut()
 
 if __name__ == '__main__':
     main()
